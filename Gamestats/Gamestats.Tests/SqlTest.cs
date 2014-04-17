4e7003738ab9aad87b2756980def7c78ae18422c
Baptista <Bruno_baptista86@hotmail.com>
2014-04-17 19:22:32 +0100

    Appharbor issue

--- Gamestats/Gamestats.Tests/SqlTest.cs
+++ Gamestats/Gamestats.Tests/SqlTest.cs
@@ -19,6 +19,7 @@ namespace Gamestats.Tests
             conn.ConnectionString = "Server=2c561a59-23ab-4f93-b2a2-a310010e1526.sqlserver.sequelizer.com;Database=db2c561a5923ab4f93b2a2a310010e1526;User ID=qbbzknotjbnefvmn;Password=4aMjMmn7rgNoFoV3oF8kzhySBYyGuesRbVTEC3NzWfNrZuohoM5MYsgUJ4QWVNKm;";
                 //"Data Source=BAPTISTA;Initial Catalog=Gamestats;Integrated Security=True";
             conn.Open();
+            Assert.IsNotNull(conn);
             return conn;
         }
 
@@ -45,6 +46,7 @@ namespace Gamestats.Tests
 
                 reader.Close();
             }
+            Assert.IsNotNull(list);
             return list;
         }
     }
